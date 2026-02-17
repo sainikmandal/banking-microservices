604625b6ef187b34f95c28ef5816c1af5cf4d487
sainikm <isainikkhan@gmail.com>
2026-02-17 20:43:59 +0530

    Add cloud API gateway

---
+++ cloudapigateway/src/main/java/com/siemens/cloudapigateway/CloudapigatewayApplication.java
@@ -0,0 +1,15 @@
+package com.siemens.cloudapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CloudapigatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudapigatewayApplication.class, args);
+    }
+
+}
---
+++ cloudapigateway/src/test/java/com/siemens/cloudapigateway/CloudapigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siemens.cloudapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudapigatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
